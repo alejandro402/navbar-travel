e2787a8cde82999563da87b9bd8cd8d7f521db1e
Alejandro <62778102+alejandro402@users.noreply.github.com>
2022-06-20 20:54:00 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    
+    <link rel="stylesheet" href="style.css">
+    <title>Travel</title>
+</head>
+<body>
+    
+    <section class="header">
+
+        <a href="home.php" class="logo">Travel.</a>
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars">
+
+        </div>
+
+    </section>
+
+    <div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+        <h1>about us</h1>
+    </div>
+
+
+    <section class="footer">
+
+        <div class="box-container">
+            <div class="box">
+                <h3>Quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+            </div>
+
+            <div class="box">
+                <h3>Extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i>policy privacy</a>
+                <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i>+15252555</a>
+                <a href="#"><i class="fas fa-phone"></i>+15252455</a>
+                <a href="#"><i class="fas fa-envelope"></i>shaikhamas@gamil.com</a>
+                <a href="#"><i class="fas fa-map"></i>cali , colombia</a>
+            </div>
+
+            <div class="box">
+                <h3>Follow us</h3>
+                <a href="#"><i class="fab fa-facebook-f">Facebook</i></a>
+                <a href="#"><i class="fab fa-twitter">twitter</i></a>
+                <a href="#"><i class="fab fa-instagram">instagram</i></a>
+                <a href="#"><i class="fab fa-linkedin">linkedin</i></a>
+            </div>
+
+
+        </div>
+
+        <div class="credit">created by <span>mr web </span>derechos reservados</div>
+
+
+    </section>
+
+
+
+    <script src="https://unpkg.com/swiper@7/swiper-bundle-min.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
